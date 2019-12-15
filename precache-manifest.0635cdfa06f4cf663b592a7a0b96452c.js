589a7cad4e9a17cb7bb23eccbba6178f10e3250b
Jeremiah Yerxa <Jeremiah.Yerxa@gmail.com>
2019-12-15 12:01:01 -0500

    Updates

---
+++ precache-manifest.0635cdfa06f4cf663b592a7a0b96452c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c739ef94b0a0c8c7e6f90e5f2479a57",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "70243d69a63be8ec692b",
+    "url": "/monsters-rolodex/static/css/main.8ddc1cb3.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70243d69a63be8ec692b",
+    "url": "/monsters-rolodex/static/js/main.830e0959.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
